3646fa4fbbdd1ee96b95b735320e6125fc41a8cd
radhinghaida <radhinghaida03@gmail.com>
2020-08-31 19:05:44 +0700

    insyaallah paling fix ini

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+const toTop = document.querySelector(".to-top");
+
 window.addEventListener('scroll', event => {
     let nav = document.querySelector('.header');
 
     (window.scrollY >= 150) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
+})
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 150) {
+        toTop.classList.add("active");
+    } else{
+        toTop.classList.remove("active");
+    }
 })
